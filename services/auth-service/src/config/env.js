7e285959d2371c0e254bc5720ebb8fc8fdbfc6cc
chhualsinghjanmejay2003-tech <chhualsinghjanmejay2003@gmail.com>
2026-08-14 15:50:22 +0530

    update env.js

--- services/auth-service/src/config/env.js
+++ services/auth-service/src/config/env.js
@@ -2,7 +2,19 @@ require("dotenv").config();
 
 const env = {
     nodeEnv: process.env.NODE_ENV || "development",
+
     port: Number(process.env.AUTH_SERVICE_PORT) || 3001,
+
+    jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
+    jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
 };
+
+if (!env.jwtAccessSecret) {
+    throw new Error("JWT_ACCESS_SECRET is required");
+}
+
+if (!env.jwtRefreshSecret) {
+    throw new Error("JWT_REFRESH_SECRET is required");
+}
 
 module.exports = env;
