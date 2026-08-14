a80591d5bf9fc43f25309d5b30952356ca54e919
chhualsinghjanmejay2003-tech <chhualsinghjanmejay2003@gmail.com>
2026-08-14 17:50:35 +0530

    implement MongoDB connection and server startup logic; add logger utility

--- services/auth-service/src/server.js
+++ services/auth-service/src/server.js
@@ -1,8 +1,20 @@
 const app = require("./app");
 const env = require("./config/env");
+const connectDB = require("./config/database");
 
-app.listen(env.port, () => {
-    console.log(
-        `${env.nodeEnv} auth-service running on port ${env.port}`
-    );
-});
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(env.port, () => {
+            console.log(
+                `${env.nodeEnv} auth-service running on port ${env.port}`
+            );
+        });
+    } catch (error) {
+        console.error("Failed to start auth-service");
+        process.exit(1);
+    }
+};
+
+startServer();
