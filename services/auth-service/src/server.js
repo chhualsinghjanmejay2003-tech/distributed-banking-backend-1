1c4844dce9ac6860d196d5345e62b68fe82a1f7b
chhualsinghjanmejay2003-tech <chhualsinghjanmejay2003@gmail.com>
2026-08-22 22:57:57 +0530

    implement graceful shutdown logic and enhance server startup handling

--- services/auth-service/src/server.js
+++ services/auth-service/src/server.js
@@ -2,12 +2,15 @@ const app = require("./app");
 const env = require("./config/env");
 const connectDB = require("./config/database");
 const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+let server;
 
 const startServer = async () => {
     try {
         await connectDB();
 
-        app.listen(env.port, () => {
+        server = app.listen(env.port, () => {
             logger.info(
                 `${env.nodeEnv} auth-service running on port ${env.port}`
             );
@@ -21,5 +24,40 @@ const startServer = async () => {
         process.exit(1);
     }
 };
+
+const gracefulShutdown = async (signal) => {
+    logger.info({ signal }, "Shutdown signal received");
+
+    if (!server) {
+        process.exit(0);
+    }
+
+    server.close(async () => {
+        logger.info("HTTP server closed");
+
+        try {
+            await mongoose.connection.close();
+
+            logger.info("MongoDB connection closed");
+
+            process.exit(0);
+        } catch (error) {
+            logger.error(
+                { error: error.message },
+                "Error during shutdown"
+            );
+
+            process.exit(1);
+        }
+    });
+};
+
+process.on("SIGINT", () => {
+    gracefulShutdown("SIGINT");
+});
+
+process.on("SIGTERM", () => {
+    gracefulShutdown("SIGTERM");
+});
 
 startServer();
