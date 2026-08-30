edb9f270441449a41812a05e3ec0276b69181744
chhualsinghjanmejay2003-tech <chhualsinghjanmejay2003@gmail.com>
2026-08-30 16:26:59 +0530

    refactor(jwt.js): improve code formatting and structure for verifyAccessToken function

--- services/account-service/src/utils/jwt.js
+++ services/account-service/src/utils/jwt.js
@@ -1,12 +1,25 @@
 const jwt = require("jsonwebtoken");
-const env = require("../config/env");
+
+const env = require(
+    "../config/env"
+);
+
+
+const JWT_ALGORITHM = "HS256";
+
 
 const verifyAccessToken = (token) => {
     return jwt.verify(
         token,
-        env.jwtSecret
+        env.jwtSecret,
+        {
+            algorithms: [
+                JWT_ALGORITHM,
+            ],
+        }
     );
 };
+
 
 module.exports = {
     verifyAccessToken,
